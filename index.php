98389cb8f049fce2732e323689f72183f97d7931
rony.almiron.015@gmail.com <rony.almiron.015@gmail.com>
2020-06-19 23:43:35 -0300

    version 0.1

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cons-Prof</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="shortcut icon" href="img/lOGO-CAMARA.png" type="image/x-icon">
+</head>
+
+<body>
+<section id="contenido">
+    <!--Inicio de Cabecera-->
+    <section id="cabecera">
+        <header id="header">
+            
+              <img src="img/lOGO-2.png" alt="" id="logoimg">  
+            
+        </header>
+        <div class="clearfix"></div>
+        <nav id="menu">
+            <ul>
+                <li><a href="#">MENU</a></li>
+                <li><a href="#">ACERCA DE CONSPROF</a></li>
+                <li><a href="#">CONTACTO</a></li>
+                <li>
+                    <a href="#">INICIAR SESIÓN</a>
+                    <ul>
+                        <li>
+                            <a href="Cliente/login.php">ACCESO CLIENTE</a>
+                        </li>
+                        <li>
+                            <a href="Profesional/login.php">ACCESO PROFESIONAL</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </section>
+    <!--Fin de Cabecera-->
+
+    <!--Inicio del Footer-->
+    <div class="clearfix"></div>
+    <footer id="footer">
+        <p>&copy; Todos los derechos reservado</p>
+        <p>CONSPROF</p>
+    </footer>
+    <!--Fin del Footer-->
+
+
+</section>
+
+   
+
+</body>
+
+</html>
