6af1ff142b0573ca5feca257dcd3bd9fefb3f4fc
rony.almiron.015@gmail.com <rony.almiron.015@gmail.com>
2020-06-20 01:57:27 -0300

    ver

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                     <a href="#">INICIAR SESIÓN</a>
                     <ul>
                         <li>
-                            <a href="https://cons-prof.herokuapp.com/lables.php">ACCESO CLIENTE</a>
+                            <a href="Cliente/login.php">ACCESO CLIENTE</a>
                         </li>
                         <li>
                             <a href="Profesional/login.php">ACCESO PROFESIONAL</a>
